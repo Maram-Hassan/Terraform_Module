55eb82ccce42f5fea4c068f15456d0a178f72a0b
Maram-Hassan <maram.hassan95@gmail.com>
2024-11-17 13:29:11 +0200

    Initial commit for terraform module project

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -10,15 +10,11 @@ resource "aws_db_instance" "mysql" {
   password               = "admin123"
   skip_final_snapshot    = true
   publicly_accessible    = false
-  # vpc_security_group_ids = [aws_security_group.db_sg.id]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
     tags = {
     Name = var.db_name
   }
 }
-# # DB Subnet Group
-# resource "aws_db_subnet_group" "my_db_subnet_group" {
-#   name       = "my-db-subnet-group"
-#   subnet_ids = [aws_subnet.private_subnet.id]
-# }
+
 
 
